9880857dcd8d1bed26753cbae1f2219294978f75
Manfred Mayer <mmayer@web.de>
2006-10-07 17:11:06 +0000

    INIT
    git-svn-id: http://svn.macrodeck.com/macrodeck-sync/trunk@240 22037474-b016-0410-8482-8abe5be14c89

---
+++ de.mmeggs.macrodeck.browser.firefox/src/de/mmeggs/macrodeck/browser/firefox/actions/BrowserSyncAction.java
@@ -0,0 +1,36 @@
+package de.mmeggs.macrodeck.browser.firefox.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+import de.mmeggs.macrodeck.lca.actions.SyncAction;
+
+/**
+ * Our sample action implements workbench action delegate.
+ * The action proxy will be created by the workbench and
+ * shown in the UI. When the user tries to use the action,
+ * this delegate will be created and execution will be 
+ * delegated to it.
+ * @see IWorkbenchWindowActionDelegate
+ */
+public class BrowserSyncAction extends SyncAction {
+	/**
+	 * The constructor.
+	 */
+	public BrowserSyncAction() {
+	}
+
+	/**
+	 * The action has been activated. The argument of the
+	 * method represents the 'real' action sitting
+	 * in the workbench UI.
+	 * @see IWorkbenchWindowActionDelegate#run
+	 */
+	public void run(IAction action) {
+		MessageDialog.openInformation(
+			window.getShell(),
+			"Firefox Plug-in",
+			"Sync done");
+	}
+}
