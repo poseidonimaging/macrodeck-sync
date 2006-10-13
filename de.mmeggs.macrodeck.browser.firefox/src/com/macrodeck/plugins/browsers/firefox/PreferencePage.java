fdb233f6010688a5478bcfdd146a2c71c9b85842
Manfred Mayer <mmayer@web.de>
2006-10-13 22:09:37 +0000

    renamings
    git-svn-id: http://svn.macrodeck.com/macrodeck-sync/trunk@272 22037474-b016-0410-8482-8abe5be14c89

---
+++ de.mmeggs.macrodeck.browser.firefox/src/com/macrodeck/plugins/browsers/firefox/PreferencePage.java
@@ -0,0 +1,43 @@
+package com.macrodeck.plugins.browsers.firefox;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+import com.macrodeck.lca.sync.ui.settings.EmptyPreferencePage;
+
+
+public class PreferencePage extends EmptyPreferencePage {
+
+	public static String[] CHECK_LABELS = new String[] {
+		"Bookmarks", "Cookies", "Profile" };
+	
+	protected void createMainContents(Composite mainComposite) {
+		for (int i = 0; i < CHECK_LABELS.length; i++) {
+			final int index = i;
+			final Button checkbox = new Button(mainComposite, SWT.CHECK | SWT.LEFT);
+
+			checkbox.setSelection(getPreferenceStore().getBoolean(
+					CHECK_LABELS[index].toLowerCase()));
+
+			checkbox.setText(CHECK_LABELS[index]);
+			GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+			gd.horizontalSpan = 2;
+			checkbox.setLayoutData(gd);
+			checkbox.addSelectionListener(new SelectionAdapter() {
+				public void widgetSelected(SelectionEvent e) {
+					getPreferenceStore().setValue(CHECK_LABELS[index].toLowerCase(),
+							checkbox.getEnabled());
+				}
+			});
+		}
+	}
+
+	protected IPreferenceStore doGetPreferenceStore() {
+		return Activator.getDefault().getPreferenceStore();
+	}
+}
