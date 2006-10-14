515fac0c8209b12ff8d31b2eb830518159ead1d8
Manfred Mayer <mmayer@web.de>
2006-10-14 16:35:40 +0000

    added implementation of Bookmark retrieval, sync, sha512
    git-svn-id: http://svn.macrodeck.com/macrodeck-sync/trunk@315 22037474-b016-0410-8482-8abe5be14c89

--- com.macrodeck.plugins.browsers.firefox/src/com/macrodeck/plugins/browsers/firefox/PreferencePage.java
+++ com.macrodeck.plugins.browsers.firefox/src/com/macrodeck/plugins/browsers/firefox/PreferencePage.java
@@ -13,16 +13,29 @@ import com.macrodeck.lca.sync.ui.settings.EmptyPreferencePage;
 
 public class PreferencePage extends EmptyPreferencePage {
 
-	public static String[] CHECK_LABELS = new String[] {
-		"Bookmarks", "Cookies", "Profile" };
+	public static final String P_PROFILE = "Profile";
+    public static final String P_COOKIES = "Cookies";
+    public static final String P_BOOKMARKS = "Bookmarks";
+    
+    public static String[] CHECK_LABELS = new String[] {
+		P_BOOKMARKS, P_COOKIES, P_PROFILE };
 	
+    /**
+     * set default values for prefernce properties
+     */
+    public PreferencePage() {
+        getPreferenceStore().setDefault(P_BOOKMARKS, false);
+        getPreferenceStore().setDefault(P_COOKIES, false);
+        getPreferenceStore().setDefault(P_PROFILE, false);
+    }
+    
 	protected void createMainContents(Composite mainComposite) {
 		for (int i = 0; i < CHECK_LABELS.length; i++) {
 			final int index = i;
 			final Button checkbox = new Button(mainComposite, SWT.CHECK | SWT.LEFT);
 
 			checkbox.setSelection(getPreferenceStore().getBoolean(
-					CHECK_LABELS[index].toLowerCase()));
+					CHECK_LABELS[index]));
 
 			checkbox.setText(CHECK_LABELS[index]);
 			GridData gd = new GridData(GridData.FILL_HORIZONTAL);
@@ -30,14 +43,15 @@ public class PreferencePage extends EmptyPreferencePage {
 			checkbox.setLayoutData(gd);
 			checkbox.addSelectionListener(new SelectionAdapter() {
 				public void widgetSelected(SelectionEvent e) {
-					getPreferenceStore().setValue(CHECK_LABELS[index].toLowerCase(),
+					getPreferenceStore().setValue(CHECK_LABELS[index],
 							checkbox.getEnabled());
+                    FirefoxSyncPlugin.getDefault().savePluginPreferences();     
 				}
 			});
 		}
 	}
 
 	protected IPreferenceStore doGetPreferenceStore() {
-		return Activator.getDefault().getPreferenceStore();
+		return FirefoxSyncPlugin.getDefault().getPreferenceStore();
 	}
 }
--- com.macrodeck.plugins.browsers.firefox/src/com/macrodeck/plugins/browsers/firefox/actions/BrowserSyncAction.java
+++ com.macrodeck.plugins.browsers.firefox/src/com/macrodeck/plugins/browsers/firefox/actions/BrowserSyncAction.java
@@ -1,37 +1,68 @@
 package com.macrodeck.plugins.browsers.firefox.actions;
 
+import java.io.File;
+
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
+import com.macrodeck.lca.sync.SyncPlugin;
 import com.macrodeck.lca.sync.actions.SyncAction;
-
+import com.macrodeck.plugins.browsers.firefox.FirefoxSyncPlugin;
+import com.macrodeck.plugins.browsers.firefox.PreferencePage;
+import com.macrodeck.plugins.browsers.firefox.model.Bookmarks;
 
 /**
- * Our sample action implements workbench action delegate.
- * The action proxy will be created by the workbench and
- * shown in the UI. When the user tries to use the action,
- * this delegate will be created and execution will be 
- * delegated to it.
+ * Our sample action implements workbench action delegate. The action proxy will be created by the workbench and shown in the UI.
+ * When the user tries to use the action, this delegate will be created and execution will be delegated to it.
+ * 
  * @see IWorkbenchWindowActionDelegate
  */
 public class BrowserSyncAction extends SyncAction {
-	/**
-	 * The constructor.
-	 */
-	public BrowserSyncAction() {
-	}
 
-	/**
-	 * The action has been activated. The argument of the
-	 * method represents the 'real' action sitting
-	 * in the workbench UI.
-	 * @see IWorkbenchWindowActionDelegate#run
-	 */
-	public void run(IAction action) {
-		MessageDialog.openInformation(
-			window.getShell(),
-			"Firefox Plug-in",
-			"Sync done");
-	}
+    /**
+     * The action has been activated. The argument of the method represents the 'real' action sitting in the workbench UI.
+     * 
+     * @see IWorkbenchWindowActionDelegate#run
+     */
+    public void run(IAction action) {
+        if (FirefoxSyncPlugin.getDefault().getPreferenceStore().getBoolean(PreferencePage.P_BOOKMARKS)) {
+            Bookmarks bookmarks = readBookmarksFromHTML(getProfileLocation());
+            //getDataService().sync(bookmarks);
+            MessageDialog.openInformation(this.window.getShell(), FirefoxSyncPlugin.PLUGIN_ID, "Sync done.\n"+bookmarks.toString());
+        }
+    }
+
+    private File getProfileLocation() {
+        File location = new File("D:/Dokumente und Einstellungen/m/Anwendungsdaten/Mozilla/Firefox/Profiles");
+        if (!location.exists()) {
+            SyncPlugin.getDefault().logError(FirefoxSyncPlugin.PLUGIN_ID, "Firefox profile location is incorrect");
+            // go to settings dialog ...
+        }
+        String[] list = location.list();
+        // pick first profile...
+        // offer profile selector
+        File firstProfileLocation = null;
+        if (list.length > 0) {
+            firstProfileLocation = new File(location+File.separator+list[0]);
+        }
+        return firstProfileLocation;
+    }
+
+    /**
+     * reads bookmarks from HTML file: bookmarks.html
+     * 
+     * @param profileLocation
+     * @return bookmarks object
+     */
+    private Bookmarks readBookmarksFromHTML(File profileLocation) {
+        File bookmarksFile = new File(profileLocation + File.separator+ "bookmarks.html");
+        if (!bookmarksFile.exists()) {
+            SyncPlugin.getDefault().logError(FirefoxSyncPlugin.PLUGIN_ID, "Firefox bookmarks.html file not found. Incorrect profile or newer version.");
+            return null;
+        } else {
+            return new Bookmarks(bookmarksFile);
+        }
+    }
+
 }
