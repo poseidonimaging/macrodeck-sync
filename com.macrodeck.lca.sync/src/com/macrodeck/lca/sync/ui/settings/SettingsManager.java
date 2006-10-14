09549efcb63150ca9d22ad7ba5fbc32e18af1bee
Manfred Mayer <mmayer@web.de>
2006-10-14 18:56:32 +0000

    removed pref page
    git-svn-id: http://svn.macrodeck.com/macrodeck-sync/trunk@335 22037474-b016-0410-8482-8abe5be14c89

--- com.macrodeck.lca.sync/src/com/macrodeck/lca/sync/ui/settings/SettingsManager.java
+++ com.macrodeck.lca.sync/src/com/macrodeck/lca/sync/ui/settings/SettingsManager.java
@@ -1,6 +1,10 @@
 package com.macrodeck.lca.sync.ui.settings;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.jface.preference.IPreferenceNode;
 import org.eclipse.jface.preference.PreferenceManager;
@@ -11,10 +15,13 @@ public class SettingsManager extends PreferenceManager {
 
 	public SettingsManager(PreferenceManager preferenceManager) {
 		this.preferenceManager = preferenceManager;
+        Set notDisplayedPrefPages = new HashSet();
+        notDisplayedPrefPages.add("org.eclipse.update.internal.ui.preferences.MainPreferencePage");
+        notDisplayedPrefPages.add("org.eclipse.ui.preferencePages.Workbench");
 		
 		IPreferenceNode[] nodes  = preferenceManager.getRootSubNodes();
 		for (int i = 0; i < nodes.length; i++) {
-			if (!nodes[i].getId().equals("org.eclipse.ui.preferencePages.Workbench")){
+			if (!notDisplayedPrefPages.contains(nodes[i].getId())){
 				getRoot().add(nodes[i]);	
 			}
 		} 
